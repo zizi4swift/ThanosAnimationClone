c974d63fb30f4c74b70a1ef80f347257da14ff84
setsu <zizi4swift@gmail.com>
2020-02-11 11:48:04 +0900

    AnimatableSpriteLayerを作成する

--- ThanosAnimationClone/ThanosAnimationClone/AnimatableSpriteLayer.swift
+++ ThanosAnimationClone/ThanosAnimationClone/AnimatableSpriteLayer.swift
@@ -6,4 +6,38 @@
 //  Copyright © 2020 setsu. All rights reserved.
 //
 
-import Foundation
+// Inspired by
+// https://www.calayer.com/core-animation/2018/01/05/creating-a-simple-animatable-sprite-layer.html
+
+import UIKit
+
+class AnimatableSpriteLayer: CALayer {
+    
+    private var animationValues = [CGFloat]()
+    
+    convenience init(spriteSheetImage: UIImage, spriteFrameSize: CGSize) {
+        self.init()
+        
+        // スプライトの最初のフレームのみを表示するため
+        masksToBounds = true
+        contentsGravity = .left
+        contents = spriteSheetImage.cgImage
+        bounds.size = spriteFrameSize
+        
+        // スプライトのサイズと各画像のサイズに従って画像の数を計算し、各画像のcontentsRect.origin.xオフセットを事前に計算します
+        let frameCount = Int(spriteSheetImage.size.width / spriteFrameSize.width)
+        for frameIndex in 0..<frameCount {
+            animationValues.append(CGFloat(frameIndex) / CGFloat(frameCount))
+        }
+    }
+    
+    func play() {
+        let spriteKeyframeAnimation = CAKeyframeAnimation(keyPath: "contentsRect.origin.x")
+        spriteKeyframeAnimation.values = animationValues
+        spriteKeyframeAnimation.duration = 2.0
+        spriteKeyframeAnimation.timingFunction = .init(name: .linear)
+        // valuesで指定されたキーフレーム値を使用してキーフレームアニメーションが順次変更されるようにするため
+        spriteKeyframeAnimation.calculationMode = .discrete
+        add(spriteKeyframeAnimation, forKey: nil)
+    }
+}
